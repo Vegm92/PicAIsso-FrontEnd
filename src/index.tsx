1c01c11d20184eda1bca44cd29dcf5f1447b31a4
Victor Granda <victorgranda1992@gmail.com>
2023-03-07 19:08:09 +0100

    Fix sonar files and update store

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
-import { store } from "./store";
+import { store } from "./store/store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
