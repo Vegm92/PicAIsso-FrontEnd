446dfa213e845e02dba64bb43c642ad2bc53b0df
Victor Granda <victorgranda1992@gmail.com>
2023-03-09 13:23:21 +0100

    Add mocks handlers and server files

--- src/mocks/Wrapper.tsx
+++ src/mocks/Wrapper.tsx
@@ -4,8 +4,8 @@ import { store } from "../store/store";
 interface WrapperProps {
   children: JSX.Element | JSX.Element[];
 }
-const Wrapper = ({ children }: WrapperProps): JSX.Element => {
-  return <Provider store={store}>{children}</Provider>;
-};
+const Wrapper = ({ children }: WrapperProps): JSX.Element => (
+  <Provider store={store}>{children}</Provider>
+);
 
 export default Wrapper;
