07fcc6d9de935f331614f4b934a670687b562778
Victor Granda <victorgranda1992@gmail.com>
2023-03-09 13:19:09 +0100

    Refactor useUser and extracted modals for latter use

--- src/hooks/useUser/types.ts
+++ src/hooks/useUser/types.ts
@@ -1,9 +1,5 @@
 import { JwtPayload } from "jwt-decode";
-
-export interface UserCredentials {
-  username: string;
-  password: string;
-}
+import { UserCredentials } from "../../store/features/users/usersSlice/types";
 
 export interface LoginResponse {
   token: string;
@@ -11,5 +7,11 @@ export interface LoginResponse {
 
 export interface CustomTokenPayload extends JwtPayload {
   id: string;
+  email: string;
   username: string;
+}
+
+export interface UseUserStructure {
+  loginUser: (userCredentials: UserCredentials) => Promise<void>;
+  logoutUser: () => void;
 }
--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,65 +1,46 @@
 import decodeToken from "jwt-decode";
 import { useAppDispatch } from "../../store/hooks";
 import useToken from "../useToken/useToken";
-import { CustomTokenPayload, LoginResponse, UserCredentials } from "./types";
-import { User } from "../../store/features/users/usersSlice/types";
+import { CustomTokenPayload, LoginResponse, UseUserStructure } from "./types";
 import {
   loginUserActionCreator,
   logoutUserActionCreator,
 } from "../../store/features/users/usersSlice/usersSlice";
-import { LoginErrorModal } from "../../modals/LoginErrorModal";
-import { LoginSuccessModal } from "../../modals/LoginSuccessModal";
-import { LogoutSuccessModal } from "../../modals/LogoutSuccessModal";
-
-interface UseUserStructure {
-  loginUser: (userCredentials: UserCredentials) => Promise<void>;
-  logoutUser: () => void;
-}
+import {
+  UserCredentials,
+  UserState,
+} from "../../store/features/users/usersSlice/types";
 
 const useUser = (): UseUserStructure => {
   const dispatch = useAppDispatch();
   const { removeToken } = useToken();
 
-  const apiUrl = process.env.REACT_APP_URL_API;
-  const usersEndpoint = "users/";
-  const loginEndpoint = "login/";
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const usersEndpoint = "/users";
+  const loginEndpoint = "/login";
 
   const loginUser = async (userCredentials: UserCredentials) => {
-    try {
-      const response = await fetch(
-        `${apiUrl}${usersEndpoint}${loginEndpoint}`,
-        {
-          method: "POST",
-          body: JSON.stringify(userCredentials),
-          headers: { "Content-Type": "application/json" },
-        }
-      );
+    const response = await fetch(`${apiUrl}${usersEndpoint}${loginEndpoint}`, {
+      method: "POST",
+      body: JSON.stringify(userCredentials),
+      headers: { "Content-Type": "application/json" },
+    });
 
-      const { token } = (await response.json()) as LoginResponse;
+    const { token } = (await response.json()) as LoginResponse;
 
-      const tokenPayload: CustomTokenPayload = decodeToken(token);
+    const tokenPayload: CustomTokenPayload = decodeToken(token);
 
-      const { id, username } = tokenPayload;
+    const { username } = tokenPayload;
+    const logginUser: UserState = { token, username, isLogged: false };
 
-      const logginUser: User = {
-        username,
-        token,
-        id,
-      };
-      dispatch(loginUserActionCreator(logginUser));
+    dispatch(loginUserActionCreator(logginUser));
 
-      localStorage.setItem("token", token);
-
-      LoginSuccessModal();
-    } catch (error) {
-      LoginErrorModal();
-    }
+    localStorage.setItem("token", token);
   };
 
   const logoutUser = () => {
     removeToken();
     dispatch(logoutUserActionCreator());
-    LogoutSuccessModal();
   };
 
   return { loginUser, logoutUser };
