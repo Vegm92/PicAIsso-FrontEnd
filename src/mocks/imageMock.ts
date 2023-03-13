c41b33a658c2c0915ce5713d92d27278d9252c7b
Victor Granda <victorgranda1992@gmail.com>
2023-03-13 23:55:58 +0100

    Add imageMocks for testing and modify token payload useUser

--- src/mocks/imageMock.ts
+++ src/mocks/imageMock.ts
@@ -1,7 +1,7 @@
 import { ImageDataStructure } from "../types/imagesTypes";
 import img from "../media/Chameleon_TradingCard.webp";
 
-const imageMock: ImageDataStructure = {
+export const imageMockCard: ImageDataStructure = {
   title: "Abstract Chameleon",
   category: ["whatever", "AnEver"],
   description: "This is an abstract Chameleon",
@@ -10,4 +10,19 @@ const imageMock: ImageDataStructure = {
   id: "qwert12345",
 };
 
-export default imageMock;
+export const imageMock: ImageDataStructure = {
+  title: "Abstract Chameleon",
+  category: ["whatever", "AnEver"],
+  description: "This is an abstract Chameleon",
+  prompt: "Draw a colorfull abstract chameleon",
+  image: "mockimage.png",
+  id: "qwert12345",
+};
+export const imageMockVariation: ImageDataStructure = {
+  title: "Abstract mock",
+  category: ["whatever", "AnEver"],
+  description: "This is an abstract Chameleon",
+  prompt: "Draw a colorfull abstract chameleon",
+  image: "mockimage.png",
+  id: "qwert12345",
+};
