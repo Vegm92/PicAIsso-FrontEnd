58f07330d54b911e84b574c5ca78a838471c9e2b
Victor Granda <victorgranda1992@gmail.com>
2023-03-22 03:19:25 +0100

    Add loadImageById, and its test

--- src/store/features/imagesSlice/imagesSlice.test.tsx
+++ src/store/features/imagesSlice/imagesSlice.test.tsx
@@ -8,35 +8,56 @@ import {
   imagesReducer,
   loadImagesActionCreator,
 } from "./imagesSlice";
-import { ImagesData, ImagesDataStructure } from "../../../types/imagesTypes";
+import {
+  ImagesData,
+  ImagesDataStructure,
+  ImagesFromApi,
+} from "../../../types/imagesTypes";
 
 const images: ImagesDataStructure = [imageMock, imageMockVariation];
-const initialImagesState: ImagesData = {
+const initialImagesState: ImagesFromApi = {
   images: [],
+  image: {
+    id: "",
+    title: "",
+    description: "",
+    category: "",
+    userPrompt: "",
+    image: "",
+    promptedBy: "",
+  },
 };
 
 describe("Given a users reducer", () => {
   describe("When it receives a new state and the action to load events", () => {
     test("Then it should return a list of 2 events", () => {
       const loadImagesAction = loadImagesActionCreator(images);
-      const expectecImagesToRender: ImagesData = {
-        images: images,
-      };
 
       const newImages = imagesReducer(initialImagesState, loadImagesAction);
 
-      expect(newImages).toStrictEqual(expectecImagesToRender);
+      const updatedImages = {
+        ...initialImagesState,
+        images: images,
+      };
+      expect(updatedImages).toStrictEqual(newImages);
     });
   });
 
   describe("When it receives a new state and the action to delete events", () => {
     test("Then it should return an event", () => {
-      const deleteImageAction = deleteImagesActionCreator(imageMockVariation);
-      const expectedNewArray: ImagesData = { images: [imageMock] };
+      const initialImagesToDelete: ImagesFromApi = {
+        ...initialImagesState,
+        images: images,
+      };
 
-      const deletedEvent = imagesReducer(mockImages, deleteImageAction);
+      const deleteImagesAction = deleteImagesActionCreator(images[0].id);
+      const result = imagesReducer(initialImagesToDelete, deleteImagesAction);
+      const updatedImages = {
+        ...initialImagesState,
+        images: [imageMockVariation],
+      };
 
-      expect(deletedEvent).toStrictEqual(expectedNewArray);
+      expect(updatedImages).toStrictEqual(result);
     });
   });
 });
